2b18d3002b6161f7bdd9de90a888191b13fe8e3d
PhongHua <phonghua1989@hotmail.com>
2022-03-01 17:38:32 +1100

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e!&+buvma&1g69wzrs4z(4^60k+^*h$^a0@yo$c8-h)d+abzy)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -125,3 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "profiles_api.UserProfile"
+
+STATIC_ROOT = 'static/'
